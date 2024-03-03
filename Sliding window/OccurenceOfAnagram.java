d049cf10a254e649dce5e9826ed7dced5be5425b
Ritik <negiritik47@gmail.com>
2024-03-03 23:24:26 +0530

    first code

---
+++ Sliding window/OccurenceOfAnagram.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class OccurenceOfAnagram {
+    public boolean allZero(int[] count) {
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int search(String pat, String txt) {
+        int k = pat.length();
+        int[] count = new int[26];
+        for (char ch : pat.toCharArray()) {
+            count[ch - 'a']++;
+        }
+        int i = 0, j = 0;
+        int n = txt.length();
+        int result = 0;
+        while (j < n) {
+            int idx = txt.charAt(j) - 'a';
+            count[idx]--;
+            if (j - i + 1 == k) {
+                if (allZero(count)) {
+                    result++;
+                }
+                count[txt.charAt(i) - 'a']++;
+                i++;
+            }
+            j++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        OccurenceOfAnagram OccurenceOfAnagram = new OccurenceOfAnagram();
+        String txt = "forxxorfxdofr";
+        String pat = "for";
+        int result = OccurenceOfAnagram.search(pat, txt);
+        System.out.println("Number of '" + pat + "' anagrams in '" + txt + "' is: " + result);
+    }
+}
